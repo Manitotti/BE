c5a6f8bd670170e8ec2bfda09748ef1c03fd3c4d
wldh25 <liho858@gmail.com>
2025-05-03 07:21:09 +0900

    초기 설정

---
+++ src/main/java/com/manittotie/manilib/ManilibApplication.java
@@ -0,0 +1,13 @@
+package com.manittotie.manilib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManilibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManilibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manittotie/manilib/ManilibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manittotie.manilib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManilibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
