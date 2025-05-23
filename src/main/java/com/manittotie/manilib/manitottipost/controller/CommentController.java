7df630998802021e583807d4d406c2e7bbd1716c
Yebin0319 <kuyebin@naver.com>
2025-05-23 18:16:31 +0900

    Feat: Add Delete Comment

--- src/main/java/com/manittotie/manilib/manitottipost/controller/CommentController.java
+++ src/main/java/com/manittotie/manilib/manitottipost/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.manittotie.manilib.manitottipost.controller;
 
 import com.manittotie.manilib.auth.dto.CustomUserDetails;
+import com.manittotie.manilib.groups.dto.MessageResponse;
 import com.manittotie.manilib.manitottipost.dto.AddCommentRequest;
 import com.manittotie.manilib.manitottipost.dto.AddCommentResponse;
 import com.manittotie.manilib.manitottipost.service.CommentService;
@@ -9,10 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/comments")
@@ -30,5 +28,15 @@ public class CommentController {
         AddCommentResponse response = commentService.createComment(request, member);
 
         return ResponseEntity.ok(response);
+    }
+
+
+    @Operation(summary = "댓글 삭제 API", description = "댓글을 삭제하는 API입니다.")
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<?> deleteComment(
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal CustomUserDetails memberDetails) {
+        commentService.deleteComment(commentId, memberDetails.getMember());
+        return ResponseEntity.ok(new MessageResponse("댓글이 성공적으로 삭제되었습니다."));
     }
 }
--- src/main/java/com/manittotie/manilib/manitottipost/service/CommentService.java
+++ src/main/java/com/manittotie/manilib/manitottipost/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.manittotie.manilib.manitottipost.service;
 
+import com.manittotie.manilib.groups.domain.Groups;
 import com.manittotie.manilib.manitottipost.domain.ManitottiComment;
 import com.manittotie.manilib.manitottipost.domain.ManitottiPost;
 import com.manittotie.manilib.manitottipost.dto.AddCommentRequest;
@@ -9,7 +10,9 @@ import com.manittotie.manilib.manitottipost.repository.ManipostRepository;
 import com.manittotie.manilib.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -20,6 +23,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final ManipostRepository manipostRepository;
 
+    // 댓글 생성
     public AddCommentResponse createComment(AddCommentRequest request, Member member) {
         ManitottiPost post = manipostRepository.findById(request.getManiPostId())
                 .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
@@ -51,6 +55,21 @@ public class CommentService {
 
         return response;
     }
+
+    // 댓글 삭제
+    @Transactional
+    public void deleteComment(Long commentId, Member member) {
+
+        ManitottiComment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다"));
+
+        if(!comment.getMember().getId().equals(member.getId())) {
+            throw new AccessDeniedException("삭제 권한이 없습니다.");
+        }
+
+        commentRepository.delete(comment);
+    }
+
 
 
 
