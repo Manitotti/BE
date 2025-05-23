8f7440e89369c6ce4e3240183c4ae2468746bfa4
Yebin0319 <kuyebin@naver.com>
2025-05-23 20:01:00 +0900

    Feat: Add Update Comment close #39

--- src/main/java/com/manittotie/manilib/manitottipost/controller/CommentController.java
+++ src/main/java/com/manittotie/manilib/manitottipost/controller/CommentController.java
@@ -4,6 +4,7 @@ import com.manittotie.manilib.auth.dto.CustomUserDetails;
 import com.manittotie.manilib.groups.dto.MessageResponse;
 import com.manittotie.manilib.manitottipost.dto.AddCommentRequest;
 import com.manittotie.manilib.manitottipost.dto.AddCommentResponse;
+import com.manittotie.manilib.manitottipost.dto.UpdateCommentRequest;
 import com.manittotie.manilib.manitottipost.service.CommentService;
 import com.manittotie.manilib.member.domain.Member;
 import io.swagger.v3.oas.annotations.Operation;
@@ -38,5 +39,15 @@ public class CommentController {
             @AuthenticationPrincipal CustomUserDetails memberDetails) {
         commentService.deleteComment(commentId, memberDetails.getMember());
         return ResponseEntity.ok(new MessageResponse("댓글이 성공적으로 삭제되었습니다."));
+    }
+
+    @Operation(summary = "댓글 수정 API", description = "댓글을 수정하는 API입니다.")
+    @PutMapping("/{commentId}")
+    public ResponseEntity<?> updateComment(
+            @PathVariable Long commentId,
+            @RequestBody UpdateCommentRequest request,
+            @AuthenticationPrincipal CustomUserDetails memberDetails) {
+        commentService.updateComment(commentId, request.getContent(), memberDetails.getMember());
+        return ResponseEntity.ok(new MessageResponse("댓글이 성공적으로 수정되었습니다."));
     }
 }
